03a98af3d0eab8a049abc20d9932e5d6d9ecb11a
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-03 01:23:21 +0330

    feat: add `CustomButton` widget for triggering advice generation
    - Created `CustomButton` widget with a tappable `InkResponse`.
    - Applied border radius using `BorderRadiusC.radius14` for consistency.
    - Used `context.exColorTheme.secondary` for dynamic theming.
    - Added elevation for better visual hierarchy.
    - Integrated `CustomButton` into `AdvicePage` for interaction.

---
+++ lib/application/pages/advice/widgets/custom_button.dart
@@ -0,0 +1,33 @@
+import 'package:clean_bloc/application/core/constants/border_radius.dart';
+import 'package:clean_bloc/application/core/extentions/extentions.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkResponse(
+      onTap: () {
+        print('clicked button');
+      },
+      child: Material(
+        borderRadius: BorderRadiusC.radius14,
+        elevation: 10,
+        child: Container(
+          decoration: BoxDecoration(
+            color: context.exColorTheme.secondary,
+            borderRadius: BorderRadiusC.radius14,
+          ),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 14),
+            child: Text(
+              'Get Advice',
+              style: context.exTextTheme.displayLarge,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
