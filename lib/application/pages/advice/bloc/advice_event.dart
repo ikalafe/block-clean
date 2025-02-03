ef689748760ee77153475d726bf5f4773143f3f4
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-04 00:43:38 +0330

    refactor: extend `AdviceEvent` with `Equatable`
    - Made `AdviceEvent` extend `Equatable` for better state comparison
    - Implemented `props` method for value-based equality
    - Added `AdviceRequestedEvent` to trigger advice retrieval

--- lib/application/pages/advice/bloc/advice_event.dart
+++ lib/application/pages/advice/bloc/advice_event.dart
@@ -1,4 +1,10 @@
 part of 'advice_bloc.dart';
 
 @immutable
-sealed class AdviceEvent {}
+sealed class AdviceEvent extends Equatable {
+
+  @override
+  List<Object?> get props => [];
+}
+
+class AdviceRequestedEvent extends AdviceEvent {}
