20ed867bb9292b44dd51393659e23479a7d89e2f
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-04 00:43:09 +0330

    feat: implement event handling in `AdviceBloc`
    - Added `AdviceRequestedEvent` to handle advice requests
    - Implemented loading state with `AdviceStateLoading`
    - Simulated advice retrieval with `Future.delayed`
    - Emitted `AdviceStateLoaded` with fake advice data
    - Introduced error handling with `AdviceStateError`

--- lib/application/pages/advice/bloc/advice_bloc.dart
+++ lib/application/pages/advice/bloc/advice_bloc.dart
@@ -1,13 +1,24 @@
-import 'package:bloc/bloc.dart';
-import 'package:meta/meta.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'advice_event.dart';
+
 part 'advice_state.dart';
 
 class AdviceBloc extends Bloc<AdviceEvent, AdviceState> {
   AdviceBloc() : super(AdviceInitial()) {
-    on<AdviceEvent>((event, emit) {
-      // TODO: implement event handler
+    on<AdviceRequestedEvent>((event, emit) async {
+      emit(AdviceStateLoading());
+      // execute business logic
+      // for example get and advice
+      debugPrint('Fake get advice triggered');
+      await Future.delayed(const Duration(seconds: 3), () {});
+      debugPrint('got advice');
+
+      emit(AdviceStateLoaded(advice: 'Fake Advice to test bloc'));
+      emit(AdviceStateError(message: 'Error message'));
+      // emit(AdviceStateError(message: 'Error message'));
     });
   }
 }
