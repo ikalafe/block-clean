a63a3de643698fee698e701cb21df62cdb46099a
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-04 00:44:56 +0330

    feat: define `AdvicerState` for cubit
    - Created `AdvicerState` as a sealed class extending `Equatable`
    - Implemented `AdvicerInitial` as the default state
    - Ensured proper state comparison with `props` override

---
+++ lib/application/pages/advice/cubit/advicer_state.dart
@@ -0,0 +1,10 @@
+part of 'advicer_cubit.dart';
+
+sealed class AdvicerState extends Equatable {
+  const AdvicerState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class AdvicerInitial extends AdvicerState {}
