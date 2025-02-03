633bd9a6baaff6d57aeed6e5fe7261f8726bfe3e
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-04 01:01:09 +0330

    refactor: update `AdvicerCubitState` structure
    - Renamed `AdvicerState` to `AdvicerCubitState`
    - Added `AdviceStateLoading`, `AdviceStateLoaded`, and `AdviceStateError`
    - Implemented `Equatable` for efficient state comparison

--- lib/application/pages/advice/cubit/advicer_state.dart
+++ lib/application/pages/advice/cubit/advicer_state.dart
@@ -1,10 +1,29 @@
 part of 'advicer_cubit.dart';
 
-sealed class AdvicerState extends Equatable {
-  const AdvicerState();
+sealed class AdvicerCubitState extends Equatable {
+  const AdvicerCubitState();
 
   @override
-  List<Object> get props => [];
+  List<Object?> get props => [];
 }
 
-final class AdvicerInitial extends AdvicerState {}
+final class AdviceInitial extends AdvicerCubitState {}
+
+final class AdviceStateLoading extends AdvicerCubitState {}
+
+final class AdviceStateLoaded extends AdvicerCubitState {
+  final String advice;
+
+  AdviceStateLoaded({required this.advice});
+
+  @override
+  List<Object?> get props => [advice];
+}
+
+final class AdviceStateError extends AdvicerCubitState {
+  final String message;
+
+  AdviceStateError({required this.message});
+  @override
+  List<Object?> get props => [message];
+}
