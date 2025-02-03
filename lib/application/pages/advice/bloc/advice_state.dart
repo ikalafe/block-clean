24a0c9c776c697dc075d70ca9128938624f2c563
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-04 00:44:09 +0330

    refactor: extend `AdviceState` with `Equatable`
    - Implemented `Equatable` for better state comparison
    - Added `AdviceStateLoading` for handling loading state
    - Introduced `AdviceStateLoaded` with an `advice` field
    - Introduced `AdviceStateError` with a `message` field

--- lib/application/pages/advice/bloc/advice_state.dart
+++ lib/application/pages/advice/bloc/advice_state.dart
@@ -1,6 +1,28 @@
 part of 'advice_bloc.dart';
 
 @immutable
-sealed class AdviceState {}
+sealed class AdviceState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
 
 final class AdviceInitial extends AdviceState {}
+
+final class AdviceStateLoading extends AdviceState {}
+
+final class AdviceStateLoaded extends AdviceState {
+  final String advice;
+
+  AdviceStateLoaded({required this.advice});
+
+  @override
+  List<Object?> get props => [advice];
+}
+
+final class AdviceStateError extends AdviceState {
+  final String message;
+
+  AdviceStateError({required this.message});
+  @override
+  List<Object?> get props => [message];
+}
