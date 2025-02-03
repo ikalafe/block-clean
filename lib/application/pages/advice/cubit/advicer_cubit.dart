466b63675273db5ebe8dd87796da0a7115e1396a
ikalafe <daniyaldehghan2021@gmail.com>
2025-02-04 01:00:23 +0330

    feat: implement advice fetching in `AdvicerCubit`
    - Added `adviceRequested` method
    - Introduced loading, success, and error states
    - Simulated advice fetching with delay

--- lib/application/pages/advice/cubit/advicer_cubit.dart
+++ lib/application/pages/advice/cubit/advicer_cubit.dart
@@ -1,8 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 
 part 'advicer_state.dart';
 
-class AdvicerCubit extends Cubit<AdvicerState> {
-  AdvicerCubit() : super(AdvicerInitial());
+class AdvicerCubit extends Cubit<AdvicerCubitState> {
+  AdvicerCubit() : super(AdviceInitial());
+
+  void adviceRequested() async {
+    emit(AdviceStateLoading());
+    // execute business logic
+    // for example get and advice
+    debugPrint('Fake get advice triggered');
+    await Future.delayed(const Duration(seconds: 3), () {});
+    debugPrint('got advice');
+
+    emit(AdviceStateLoaded(advice: 'Fake Advice to test bloc'));
+    emit(AdviceStateError(message: 'Error message'));
+    // emit(AdviceStateError(message: 'Error message'));
+  }
 }
